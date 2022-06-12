87e72445433bef75c71530fd7176018d6e6bc0c6
Onecode24 <angelo24yanve@gmail.com>
2022-06-12 23:50:04 +0100

    connect USER

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,7 +1,9 @@
 // import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
+import Database from "@ioc:Adonis/Lucid/Database";
 import User from "App/Models/User";
 import UserValidator from "App/Validators/UserValidator";
+import bcrypt = require('bcrypt')
 
 export default class UsersController {
 
@@ -19,15 +21,46 @@ export default class UsersController {
     }
   }
 
-  async login({request,auth}){
+  async login({request,response}){
     try {
 
       const {uiid,password} = request.only(['uiid','password'])
+      //
+      const data = (await Database.table('users')).values()
+      let user,hashPass;
+      while(user = data.next().value){
+        if(user.email == uiid || user.username == uiid){
+          hashPass = user.password;
+          break;
+        }
+      }
 
-      await auth.attempt(uiid,password);
+      if(user!=undefined){
+
+        const match = await bcrypt.compare(password,hashPass);
+        if(match){
+          console.log("connected");
+          response.send({
+            messge: "User connected"
+          })
+        }else{
+          response.status(404).json({
+            message: 'paswword incorrect'
+          })
+        }
+
+      }else{
+        response.status(404).json({
+          message: "username, email not found"
+        })
+      }
+
 
     } catch (error) {
       console.log(error);
+      response.status(400).json({
+        error : error
+      })
 
     }
   }
